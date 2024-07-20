69a3fa2fcea296b4cac3ea52ab414e96a6a0ea75
Marc Rijken <marc@rijken.org>
2024-07-20 21:08:27 +0200

    Update __init__.py

--- subscribe/__init__.py
+++ subscribe/__init__.py
@@ -1,3 +1,7 @@
+import warnings
+
+warnings.warn("This package is not maintained anymore", DeprecationWarning)
+
 from subscribe.subscribers import Command, Event  # noqa
 from subscribe.subscriptions import (  # noqa
     ClassSubscriptionList,
